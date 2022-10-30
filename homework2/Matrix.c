b3e8d1bde26af912f25d0b7a096e77ed0cb65fef
cgy <948628463@qq.com>
2022-10-30 21:03:08 +0800

    homework2

--- homework2/Matrix.c
+++ homework2/Matrix.c
@@ -1,193 +1,315 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <pthread.h>
-#include <stdarg.h>
 #include <immintrin.h>
 
-const int test_row = 1000;
-const int test_col = 1000;
 const int thread_count = 10;
+const int row = 1000;
+const int col = 1000;
 
-typedef struct{
-    int row;
-    int col;
-    double** m;
-}Matrix;
+float* matrix1; 
+float* matrix2;
+float* plus_res;
+float* minus_res;
+float* multiply_res;
 
-struct params{
-    int tid;
-    Matrix* res;
-    Matrix* m1;
-    Matrix* m2;
-};
+void check_plus();
+void check_minus();
+void check_multiply();
 
-void matrix_init(Matrix* m, int row, int col){
-    m->row = row;
-    m->col = col;
-    m->m = (double**)malloc(m->row * sizeof(double*));
-    for(int r=0; r<m->row; r++){
-        m->m[r] = (double*)calloc(m->col, sizeof(double));
+void matrix_init();
+void matrix_destroy();
+
+void matrix_minus_normal();
+void matrix_plus_normal();
+void matrix_multiply_normal();
+    
+void matrix_plus_simd();
+void matrix_minus_simd();
+void matrix_multiply_simd();
+
+void matrix_plus_omp();
+void matrix_minus_omp();
+void matrix_multiply_omp();
+
+void matrix_plus_pth();
+void matrix_minus_pth();
+void matrix_multiply_pth();
+void* __matrix_plus_pth_divider();
+void* __matrix_minus_pth_divider();
+void* __matrix_multiply_pth_divider();
+
+int main(int argc, char** argv){
+    if(argc == 2){
+        matrix_init();
+        if(strcmp(argv[1], "normal") == 0){
+            // normal
+            matrix_minus_normal();
+            matrix_plus_normal();
+            matrix_multiply_normal();
+        }else if(strcmp(argv[1], "simd") == 0){        
+            // simd
+            matrix_plus_simd();
+            matrix_minus_simd();
+            matrix_multiply_simd();
+        }else if(strcmp(argv[1], "omp") == 0){
+            // omp
+            matrix_plus_omp();
+            matrix_minus_omp();
+            matrix_multiply_omp();
+        }else if(strcmp(argv[1], "pth") == 0){
+            // pth
+            matrix_plus_pth();
+            matrix_minus_pth();
+            matrix_multiply_pth();
+        }
+        
+        // check_plus();
+        // check_minus();
+        // check_multiply();
+
+        matrix_destroy();
+    }else{
+        printf("please choose a mode:\n");
+        printf("  normal\n");
+        printf("  simd\n");
+        printf("  omp\n");
+        printf("  pth\n");
     }
 }
 
-void matrix_set(Matrix* m, ...){
-    va_list v;
-    va_start(v, m);
-    double value;
-    for(int r=0; r<m->row; r++){
-        for(int c=0; c<m->col; c++){
-            value = va_arg(v, double);
-            m->m[r][c] = value;
+
+void check_plus(){
+    int err = 0;
+    for(int r=0; r< row; r++){
+        for(int c=0; c< col; c++){
+            if(matrix1[r * col + c] == 2 && matrix2[r * col + c] == 2 && plus_res[r * col + c] != 4){
+                err = 1;
+                break;
+            }
+        }
+        if(err){
+            break;
         }
     }
-    va_end(v);
+    if(err){
+        printf("plus err\n");
+    }else{
+        printf("plus right\n");
+    }
 }
-
-void matrix_set_test(Matrix* m){
-    for(int r=0; r<m->row; r++){
-        for(int c=0; c<m->col; c++){
-            m->m[r][c] = 2;
+void check_minus(){
+    int err = 0;
+    for(int r=0; r< row; r++){
+        for(int c=0; c< col; c++){
+            if(matrix1[r * col + c] == 2 && matrix2[r * col + c] == 2 && minus_res[r * col + c] != 0){
+                err = 1;
+                break;
+            }
+        }
+        if(err){
+            break;
         }
     }
+    if(err){
+        printf("minus err\n");
+    }else{
+        printf("minus right\n");
+    }
 }
-
-void matrix_print(Matrix* m){
-    for(int r=0; r<m->row; r++){
-        for(int c=0; c<m->col; c++){
-            printf("%.3lf ", m->m[r][c]);
+void check_multiply(){
+    int err = 0;
+    for(int r=0; r< row; r++){
+        for(int c=0; c< col; c++){
+            if(matrix1[r * col + c] == 2 && matrix2[r * col + c] == 2 && multiply_res[r * col + c] != 4000){
+                err = 1;
+                break;
+            }
         }
-        printf("\n");
+        if(err){
+            break;
+        }
+    }
+    if(err){
+        printf("multiply err\n");
+    }else{
+        printf("multiply right\n");
     }
 }
 
-void matrix_destroy(Matrix* m){
-    for(int r=0; r<m->row; r++){
-        free(m->m[r]);
+void matrix_init(){
+    matrix1 = (float*)malloc(row * col * sizeof(float));
+    matrix2 = (float*)malloc(row * col * sizeof(float));
+    plus_res = (float*)malloc(row * col * sizeof(float));
+    minus_res = (float*)malloc(row * col * sizeof(float));
+    multiply_res = (float*)malloc(row * col * sizeof(float));
+    for(int i=0; i< row*col; i++){
+        matrix1[i] = matrix2[i] = plus_res[i] = minus_res[i] = multiply_res[i] = 2;
     }
-    free(m->m);
+}
+void matrix_destroy(){
+    free(matrix1);
+    free(matrix2);
+    free(plus_res);
+    free(minus_res);
+    free(multiply_res);
 }
 
-void matrix_multiply_normal(Matrix* res, Matrix* m1, Matrix* m2){
-    if(m1->col == m2->row && res->row == m1->row && res->col == m2->col){
-        for(int r=0; r < res->row; r++){
-            for(int c=0; c < res->col; c++){
-                res->m[r][c] = 0;
-                for(int i=0; i< m1->col; i++){
-                    res->m[r][c] += m1->m[r][i] * m2->m[i][c];
-                }
+void matrix_plus_normal(){
+    for(int i=0; i< row * col; i++){
+        plus_res[i] = matrix1[i] + matrix1[i];
+    }
+}
+void matrix_minus_normal(){
+    for(int i=0; i< row * col; i++){
+        minus_res[i] = matrix1[i] - matrix1[i];
+    }
+}
+void matrix_multiply_normal(){
+    for(int r=0; r < row; r++){
+        for(int c=0; c < col; c++){
+            multiply_res[r * col + c] = 0;
+            for(int i=0; i < col; i++){
+                multiply_res[r * col + c] += matrix1[r * col + i] * matrix2[i * col + c];
             }
         }
     }
 }
 
-void* __matrix_multiply_divider(void* args){
-    struct params* p = (struct params*)args;
-    int each = p->res->row / thread_count;
-    int start = p->tid * each;
-    int end = p->tid == thread_count-1? p->m1->row-1: (p->tid + 1) * each -1;
+void matrix_plus_simd(){
+    int i = 0;
+    int end = row * col - 8;
+    for(; i< end; i += 8){
+            __m256 v1 = _mm256_loadu_ps(matrix1 + i);
+            __m256 v2 = _mm256_loadu_ps(matrix2 + i);
+            __m256 v = _mm256_add_ps(v1, v2);
+            _mm256_storeu_ps(plus_res + i, v);
+    }
+    for(; i< row * col; i++){
+        plus_res[i] = matrix1[i] + matrix2[i];
+    }
+}
+void matrix_minus_simd(){
+    int i = 0;
+    int end = row * col - 8;
+    for(; i< end; i += 8){
+            __m256 v1 = _mm256_loadu_ps(matrix1 + i);
+            __m256 v2 = _mm256_loadu_ps(matrix2 + i);
+            __m256 v = _mm256_sub_ps(v1, v2);
+            _mm256_storeu_ps(minus_res + i, v);
+    }
+    for(; i< row * col; i++){
+        minus_res[i] = matrix1[i] - matrix2[i];
+    }
+}
+void matrix_multiply_simd(){
+    for(int r=0; r < row; r++){
+        for(int c=0; c < col; c++){
+            multiply_res[r * col + c] = 0;
+            int i;
+            for(i=0; i < col-8; i+=8){
+                __m256 v1 = _mm256_loadu_ps(matrix1 + r * col + i);
+                __m256 v2 = _mm256_set_ps(matrix2[i * col + c], matrix2[(i+1) * col + c], matrix2[(i+2) * col + c], matrix2[(i+3) * col + c],
+                                          matrix2[(i+4) * col + c], matrix2[(i+5) * col + c], matrix2[(i+6) * col + c], matrix2[(i+7) * col + c]);
+                __m256 v = _mm256_mul_ps(v1, v2);
+                float* tmp = (float*)&v;
+                multiply_res[r * col + c] += tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4] + tmp[5] + tmp[6] + tmp[7];
+            }
+            for(; i < col; i++){
+                multiply_res[r * col + c] += matrix1[r * col + i] * matrix2[i * col + c];
+            }
+        }
+    }    
+}
+
+void matrix_plus_omp(){
+    #pragma omp parallel num_threads(thread_count)
+    for(int i=0; i< row * col; i++){
+        plus_res[i] = matrix1[i] + matrix1[i];
+    }
+}
+void matrix_minus_omp(){
+    #pragma omp parallel num_threads(thread_count)
+    for(int i=0; i< row * col; i++){
+        minus_res[i] = matrix1[i] - matrix1[i];
+    }
+}
+void matrix_multiply_omp(){
+    for(int r=0; r < row; r++){
+        #pragma omp parallel num_threads(thread_count)
+        for(int c=0; c < col; c++){
+            multiply_res[r * col + c] = 0;
+            for(int i=0; i < col; i++){
+                multiply_res[r * col + c] += matrix1[r * col + i] * matrix2[i * col + c];
+            }
+        }
+    }
+}
+
+void* __matrix_plus_pth_divider(void* tid){
+    int each = row / thread_count;
+    int start = (long)tid * each;
+    int end = (long)tid == thread_count-1? row-1: ((long)tid + 1) * each -1;
     for(int r=start; r<=end; r++){
-        for(int c=0; c<p->res->col; c++){
-            p->res->m[r][c] = 0;
-            for(int i=0; i<p->m1->col; i++){
-                p->res->m[r][c] += p->m1->m[r][i] * p->m2->m[i][c];
-            }
+        for(int c=0; c<col; c++){
+            plus_res[r * col + c] = matrix1[r * col + c] + matrix2[r * col + c];
         }
     }
-    free(p);
     return NULL;
 }
-void matrix_multiply_pthread(Matrix* res, Matrix* m1, Matrix* m2){
-    if(m1->col == m2->row && res->row == m1->row && res->col == m2->col){
+void matrix_plus_pth(){
         pthread_t* thread_handles = (pthread_t*)malloc(thread_count * sizeof(pthread_t*));
         for(int tid=0; tid<thread_count; tid++){
-            struct params* p = (struct params*)malloc(sizeof(struct params));
-            p->tid = tid;
-            p->res = res;
-            p->m1 = m1;
-            p->m2 = m2;
-            pthread_create(&thread_handles[tid], NULL, __matrix_multiply_divider, (void*)p);
+            pthread_create(&thread_handles[tid], NULL, __matrix_plus_pth_divider, (long int)tid);
         }
         for(int tid=0; tid<thread_count; tid++){
             pthread_join(thread_handles[tid], NULL);
         }
         free(thread_handles);
-    }
 }
-
-void matrix_multiply_simd(Matrix* res, Matrix* m1, Matrix* m2){
-    if(m1->col == m2->row && res->row == m1->row && res->col == m2->col){
-        for(int r=0; r < res->row; r++){
-            for(int c=0; c < res->col; c++){
-                res->m[r][c] = 0;
-                int i;
-                for(i=0; i< m1->col - 4; i+=4){
-                    __m256d v1 = _mm256_set_pd(m1->m[r][i], m1->m[r][i+1], m1->m[r][i+2], m1->m[r][i+3]);
-                    __m256d v2 = _mm256_set_pd(m2->m[i][c], m2->m[i+1][c], m2->m[i+2][c], m2->m[i+3][c]);
-                    __m256d v = _mm256_mul_pd(v1, v2);
-                    double* tmp = (double*)&v;
-                    res->m[r][c] += tmp[0] + tmp[1] + tmp[2] + tmp[3];
-                }
-                for(; i< m1->col; i++){
-                    res->m[r][c] += m1->m[r][i] * m2->m[i][c];
-                }
+void* __matrix_minus_pth_divider(void* tid){
+    int each = row / thread_count;
+    int start = (long)tid * each;
+    int end = (long)tid == thread_count-1? row-1: ((long)tid + 1) * each -1;
+    for(int r=start; r<=end; r++){
+        for(int c=0; c<col; c++){
+            minus_res[r * col + c] = matrix1[r * col + c] - matrix2[r * col + c];
+        }
+    }
+    return NULL;
+}
+void matrix_minus_pth(){
+        pthread_t* thread_handles = (pthread_t*)malloc(thread_count * sizeof(pthread_t*));
+        for(int tid=0; tid<thread_count; tid++){
+            pthread_create(&thread_handles[tid], NULL, __matrix_minus_pth_divider, (long int)tid);
+        }
+        for(int tid=0; tid<thread_count; tid++){
+            pthread_join(thread_handles[tid], NULL);
+        }
+        free(thread_handles);
+}
+void* __matrix_multiply_pth_divider(void* tid){
+    int each = row / thread_count;
+    int start = (long)tid * each;
+    int end = (long)tid == thread_count-1? row-1: ((long)tid + 1) * each -1;
+    for(int r=start; r<= end; r++){
+        for(int c=0; c< col; c++){
+            multiply_res[r * col + c] = 0;
+            for(int i=0; i< col; i++){
+                multiply_res[r * col + c] += matrix1[r * col + i] * matrix2[i * col + c];
             }
         }
     }
+    return NULL;
 }
-
-void matrix_multiply_omp(Matrix* res, Matrix* m1, Matrix* m2){
-    if(m1->col == m2->row && res->row == m1->row && res->col == m2->col){
-        for(int r=0; r < res->row; r++){
-            #pragma omp parallel for num_threads(thread_count)
-            for(int c=0; c < res->col; c++){
-                res->m[r][c] = 0;
-                for(int i=0; i< m1->col; i++){
-                    res->m[r][c] += m1->m[r][i] * m2->m[i][c];
-                }
-            }
+void matrix_multiply_pth(){
+        pthread_t* thread_handles = (pthread_t*)malloc(thread_count * sizeof(pthread_t*));
+        for(int tid=0; tid<thread_count; tid++){
+            pthread_create(&thread_handles[tid], NULL, __matrix_multiply_pth_divider, tid);
         }
-    }
-}
-
-void check(Matrix* res){
-    for(int r=0; r< res->row; r++){
-        for(int c=0; c< res->col; c++){
-            if(res->m[r][c] != 4000){
-                printf("err\n");
-            }
+        for(int tid=0; tid<thread_count; tid++){
+            pthread_join(thread_handles[tid], NULL);
         }
-    }
-}
-
-int main(int argc, char** argv){
-    Matrix m1, m2, res;
-    matrix_init(&m1, test_row, test_col);
-    matrix_init(&m2, test_row, test_col);
-    matrix_init(&res, test_row, test_col);
-
-    matrix_set_test(&m1);
-    matrix_set_test(&m2);
-
-    if(argc == 2){
-        if(strcmp(argv[1], "omp") == 0){
-            matrix_multiply_omp(&res, &m1, &m2);
-        }else if(strcmp(argv[1], "simd") == 0){
-            matrix_multiply_simd(&res, &m1, &m2);
-        }else if(strcmp(argv[1], "pth") == 0){
-            matrix_multiply_pthread(&res, &m1, &m2);
-        }else if(strcmp(argv[1], "normal") == 0){
-            matrix_multiply_normal(&res, &m1, &m2);
-        }
-    }else{
-        printf("please choose a mode: \n");
-        printf("  omp\n");
-        printf("  simd\n");
-        printf("  pth\n");
-        printf("  normal\n");
-    }
-
-    matrix_destroy(&m1);
-    matrix_destroy(&m2);
-    matrix_destroy(&res);
+        free(thread_handles);
 }
