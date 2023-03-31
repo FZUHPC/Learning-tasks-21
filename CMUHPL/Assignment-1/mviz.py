478653dbcc2a75ae23bb51f9ccb863584fb464f1
consonnm <68242900@qq.com>
2023-03-31 12:32:09 +0800

    update1

--- CMUHPL/Assignment-1/mviz.py
+++
@@ -1,5 +0,0 @@
-[mandelbrot serial]:		[207.754] ms
-Wrote image file mandelbrot-v0-serial.ppm
-[mandelbrot thread]:		[39.241] ms
-Wrote image file mandelbrot-v0-thread-16.ppm
-++++				(5.29x speedup from 16 threads)
--- CMU作业/Assignment-1-/common/callibrate.h
+++
@@ -1,4 +0,0 @@
-/* For this machine.  What is the latency of floating point sum? */
-#define FLOAT_SUM_CYCLES 4
-
-double get_clockrate();
